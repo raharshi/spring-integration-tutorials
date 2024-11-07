d3e100c4df2d9f1f66a3c414484520bb933f2d99
Raharshi Puli <raharshi.puli@gmail.com>
2024-11-07 18:40:22 +0530

    queue channel added with pollers

--- src/main/java/com/spring/spring_integration_tutorials/service/OrderService.java
+++ src/main/java/com/spring/spring_integration_tutorials/service/OrderService.java
@@ -1,8 +1,10 @@
 package com.spring.spring_integration_tutorials.service;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.QueueChannel;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,20 @@ import com.spring.spring_integration_tutorials.model.Order;
 @Service
 public class OrderService {
 
-    @Bean(name = "process-order-channel")
-    public MessageChannel processOrderchannel(){
-        /*By default spring integration uses direct channel 
-        if we want to create a different channel we can use this process */
-        return new DirectChannel();
+    // @Bean(name = "process-order-channel")
+    // public MessageChannel processOrderchannel(){
+    //     /*By default spring integration uses direct channel 
+    //     if we want to create a different channel we can use this process */
+    //     return new DirectChannel();
+    // }
+
+    @Bean(name="process-order-channel")
+    public MessageChannel processChannel(){
+        return new QueueChannel(10);
+    }
+    @Bean(name="reply-order-channel")
+    public MessageChannel replyChannel(){
+        return new QueueChannel(10);
     }
 
     @ServiceActivator(inputChannel = "request-in-channel",outputChannel = "process-order-channel")
@@ -24,13 +35,13 @@ public class OrderService {
         return order;
     }
 
-    @ServiceActivator(inputChannel = "process-order-channel", outputChannel = "reply-order-channel")
+    @ServiceActivator(inputChannel = "process-order-channel", outputChannel = "reply-order-channel", poller = @Poller(fixedDelay = "100",maxMessagesPerPoll = "1"))
     public Message<Order> processOrder(Message<Order> order){
         order.getPayload().setOrderStatus("Order placed successfully!!");
         return order;
     }
 
-    @ServiceActivator(inputChannel = "reply-order-channel")
+    @ServiceActivator(inputChannel = "reply-order-channel", poller = @Poller(fixedDelay = "100",maxMessagesPerPoll = "1"))
     public void replyOrder(Message<Order> order){
         if(order.getHeaders().getReplyChannel() instanceof MessageChannel replyChannel){
             replyChannel.send(order);
